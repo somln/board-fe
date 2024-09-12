7bf67f241222bc28bc2d58c86bfd214d4337d3cb
Somin Park <ps4708@naver.com>
2024-09-12 14:17:20 +0900

    feat: key cloak 로그인 연동

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { initializeKeycloak } from './keycloak';  
+import PrivateRoute from './PrivateRoute';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
 
 function App() {
+  useEffect(() => {
+    initializeKeycloak(); 
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <h1>Welcome to the App</h1>
+      </div>
   );
 }
 
---
+++ src/keycloak.js
@@ -0,0 +1,21 @@
+import Keycloak from 'keycloak-js';
+
+const keycloak = new Keycloak({
+    url: 'http://localhost:8180',
+    realm: 'board',
+    clientId: 'main_client' 
+});
+
+const initializeKeycloak = async () => {
+    try {
+        const authenticated = await keycloak.init({ onLoad: 'login-required' }); // 'login-required'로 설정
+        console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
+        if (!authenticated) {
+            keycloak.login(); // 인증되지 않았을 경우 로그인 페이지로 리다이렉트
+        }
+    } catch (error) {
+        console.error('Failed to initialize Keycloak:', error);
+    }
+};
+
+export { keycloak, initializeKeycloak };
